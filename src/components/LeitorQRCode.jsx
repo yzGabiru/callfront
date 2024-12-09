80bfe10ff77e666bf29b77c3e7814d3683383604
Gabiru <paulohenriquegabs@gmail.com>
2024-12-09 20:55:35 -0300

    teste cameras

--- src/components/LeitorQRCode.jsx
+++ src/components/LeitorQRCode.jsx
@@ -1,7 +1,37 @@
 import PropTypes from "prop-types";
+import { useEffect, useState } from "react";
 import QrScanner from "react-qr-scanner";
 
 function LeitorQRCode({ onScan }) {
+  const [cameraId, setCameraId] = useState(null);
+
+  useEffect(() => {
+    // Verificar as câmeras disponíveis
+    const getCameras = async () => {
+      const devices = await navigator.mediaDevices.enumerateDevices();
+      const videoDevices = devices.filter(
+        (device) => device.kind === "videoinput"
+      );
+
+      // Tentar selecionar a câmera traseira
+      const rearCamera = videoDevices.find(
+        (device) =>
+          device.label.toLowerCase().includes("back") ||
+          device.label.toLowerCase().includes("environment")
+      );
+
+      // Se encontrar a câmera traseira, configurar o id
+      if (rearCamera) {
+        setCameraId(rearCamera.deviceId);
+      } else if (videoDevices.length > 0) {
+        // Caso contrário, usar a primeira câmera encontrada
+        setCameraId(videoDevices[0].deviceId);
+      }
+    };
+
+    getCameras();
+  }, []);
+
   const handleScan = (data) => {
     if (data) {
       onScan(data.text);
@@ -19,19 +49,24 @@ function LeitorQRCode({ onScan }) {
 
   return (
     <div className="space-y-4 p-20 bg-slate-200 rounded-md shadow flex flex-col">
-      <QrScanner
-        delay={300}
-        style={previewStyle}
-        onError={handleError}
-        onScan={handleScan}
-        facingMode="environment" // Define que a câmera traseira será utilizada
-      />
+      {cameraId ? (
+        <QrScanner
+          delay={300}
+          style={previewStyle}
+          onError={handleError}
+          onScan={handleScan}
+          facingMode="environment"
+          deviceId={cameraId} // Passa o id da câmera selecionada
+        />
+      ) : (
+        <p>Procurando por câmeras...</p>
+      )}
     </div>
   );
 }
 
 LeitorQRCode.propTypes = {
-  onScan: PropTypes.func.isRequired, // Define que a função onScan é obrigatória
+  onScan: PropTypes.func.isRequired,
 };
 
 export default LeitorQRCode;
